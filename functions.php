9a04d9614ccb64dd87975c9d998b28477e1a53e4
Niklas P <contact@niklasplessing.net>
2013-10-28 14:13:09 +0100

    added a second sidebar

--- functions.php
+++ functions.php
@@ -39,7 +39,7 @@ function kunscht_setup() {
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
 	 */
-	//add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'post-thumbnails' );
 
 	/**
 	 * This theme uses wp_nav_menu() in one location.
@@ -64,18 +64,28 @@ function kunscht_setup() {
 endif; // kunscht_setup
 add_action( 'after_setup_theme', 'kunscht_setup' );
 
+
+
 /**
  * Register widgetized area and update sidebar with default widgets
  */
 function kunscht_widgets_init() {
 	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'kunscht' ),
+		'name'          => __( 'Sidebar (id=sidebar-1)', 'kunscht' ),
 		'id'            => 'sidebar-1',
 		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	));
+	register_sidebar( array(
+		'name'          => __( 'Sidebar2 (id=sidebar-2)', 'kunscht' ),
+		'id'            => 'sidebar-2',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	));
     register_sidebar(array(
     'name' => __('Footer', 'kunscht'),
     'id' => 'sidebar-footer',
@@ -93,13 +103,7 @@ add_action( 'widgets_init', 'kunscht_widgets_init' );
 function kunscht_scripts() {
 	wp_enqueue_style( 'bootstrap', get_stylesheet_directory_uri() . '/bootstrap/css/bootstrap.min.css');
 	wp_enqueue_style( 'customcss', get_stylesheet_directory_uri() . '/css/kunscht.css');
-	
 	wp_enqueue_script( 'bootstrapjs', get_template_directory_uri() . '/bootstrap/js/bootstrap.min.js', array('jquery'), false, true );
-
-
-	wp_enqueue_script( 'kunscht-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'kunscht-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
